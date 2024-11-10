2cfc33046b3c73c9b05babcbbb69b1a493523c8c
codegzy-rabbit <2570713180@qq.com>
2024-11-11 00:28:09 +0800

    modify some shortcuts
    add conform & multicursor plugin

--- lua/plugins/bufexplorer.lua
+++ lua/plugins/bufexplorer.lua
@@ -1,10 +1,4 @@
 return {
   "jlanzarotta/bufexplorer",
-  dependencies = {
-    "AstroNvim/astrocore",
-    opts = function(_, opts)
-      local maps = opts.mappings
-      maps.n["<leader>o"] = { "<cmd>BufExplorer<CR>", desc = "BufExplorer" }
-    end,
-  },
+  config = function() vim.keymap.set("n", "<leader>o", "<cmd>BufExplorer<CR>", { desc = "BufExplorer" }) end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
----@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
+    require("nvim-treesitter.configs").setup {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = false,
+          node_incremental = "<CR>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
+        },
+      },
+    }
   end,
 }
