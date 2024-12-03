ff43046ead9142bce544d1e0efb029acfa2e0d12
codegzy-rabbit <2570713180@qq.com>
2024-12-04 02:03:29 +0800

    modify mini-files
    modify mini-files config
    sync shortcut

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.files",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>e"] = {
+              function()
+                if not require("mini.files").close() then require("mini.files").open() end
+              end,
+              desc = "Explorer",
+            },
+          },
+        },
+      },
+    },
+  },
+  specs = {
+    { "neo-tree.nvim", optional = true, enabled = false },
+    {
+      "catppuccin",
+      optional = true,
+      ---@type CatppuccinOptions
+      opts = { integrations = { mini = true } },
+    },
+  },
+  opts = {
+    mappings = {
+      synchronize = "W",
+    },
+  },
+}
